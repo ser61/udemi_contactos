500a9fadae691b06f2da3cd45b27aa29b1096598
sergio <ser61_ol@yahoo.es>
2017-09-10 11:35:43 -0400

    inicio

---
+++ app/src/androidTest/java/com/weiser/sergio_w/miscontactos/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.weiser.sergio_w.miscontactos;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
